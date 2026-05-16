53f30ac06b2c29b90d94a356699aa8777cc355dc
shuaijiu <2231717934@qq.com>
2026-05-16 21:31:25 +0800

    Add files via upload

---
+++ rag/qa_chain.py
@@ -0,0 +1,34 @@
+import os
+from dotenv import load_dotenv
+from openai import OpenAI
+
+load_dotenv()
+client = OpenAI(
+    api_key=os.getenv("DEEPSEEK_API_KEY"),
+    base_url="https://api.deepseek.com"
+)
+
+
+def ask_llm(query, results):
+    context = "\n".join([doc.page_content for doc in results])
+
+    prompt = f"""
+你是一名AI文档助手。
+
+请基于下面内容回答问题。
+
+文档内容：
+{context}
+
+用户问题：
+{query}
+"""
+
+    response = client.chat.completions.create(
+        model="deepseek-chat",
+        messages=[
+            {"role": "user", "content": prompt}
+        ]
+    )
+
+    return response.choices[0].message.content
